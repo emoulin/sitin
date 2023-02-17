e351dcffc2bbf7961b703ec9bd33085d95e9a43e
NogFar <nogueiramatheus@id.uff.br>
2023-02-16 23:02:38 -0300

    Primeira implementação do script
    Atualmente o script varia de 0 até o número desejado em X etapas dentro de tempo Y determinado

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,9 @@ function throttle(callback, timeout = 300) {
     }
 }
 
-const header = document.querySelector(".s-header");
+//#region  Scroll Animation
+
+const header = document.querySelector(".s-header")
 
 function scrollCheck(e){
     window.pageYOffset > 50 ? header.classList.add("shadow") : header.classList.remove("shadow");
@@ -18,7 +20,49 @@ function scrollCheck(e){
 é pouco mas diminui a quantidade de chamadas ao mesmo tempo 
 que reduz risco de não ler a última localização do scroll*/
 
-window.addEventListener('scroll', throttle(scrollCheck, 10));
+window.addEventListener("scroll", throttle(scrollCheck, 10))
+
+//#endregion Scroll Animation
+
+//#region  Counter Animation
+
+let elements = document.querySelectorAll("[data-number]");
+let steps = 10; //etapas da animação
+let timeAnimation = 500; //tempo total da animação em ms
+let delay = 500; //tempo de delay entre elementos
+let timeStep = timeAnimation/steps; //tempo entre etapas da animação
+
+for (let i = 0; i < elements.length; i++) {
+  let value = elements[i].dataset.number;
+  let prefix = elements[i].dataset.prefix;
+  let sufix = elements[i].dataset.sufix;
+  if (prefix) {
+    elements[i].textContent =  prefix + 0;
+  } else if (sufix){
+    elements[i].textContent =  "" + 0 + sufix;
+  } else {
+    elements[i].textContent =  "" + 0;
+  }
+  setTimeout(() => {
+    increaseCounter(elements[i], steps, timeStep, value, prefix, sufix);
+  }, i * delay);  
+}
+
+function increaseCounter(element, steps, time, value, prefix, sufix) {
+  for (let i = 0; i <= steps; i++) {
+    setTimeout(() => {
+      if (prefix) {
+        element.textContent =  prefix + parseInt((value/steps) * i);
+      } else if (sufix){
+        element.textContent =  "" + parseInt((value/steps) * i) + sufix;
+      } else {
+        element.textContent =  "" + parseInt((value/steps) * i);
+      }
+    }, i * time);
+  }
+}
+
+//#endregion Counter Animation
 
 
 
